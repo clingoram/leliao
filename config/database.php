c50bccf12bea848ef043e7b526f11af5c979cbeb
Mavis <llingchen.27@gmail.com>
2022-09-10 16:33:08 +0800

    edit database

--- config/database.php
+++ config/database.php
@@ -68,13 +68,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            // 'url' => env('DATABASE_URL'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '5432'),
             // 'database' => env('DB_DATABASE', 'forge'),
 
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], "/"),
-            'default' => env('DB_CONNECTION', 'pgsql'),
+            'username' => $DATABASE_URL["user"],
+            'passwrod' => $DATABASE_URL["pass"],
 
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
