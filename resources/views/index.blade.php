91f29ffd0a53451a670ffdbda87c60c9eaf2bf02
Mavis <llingchen.27@gmail.com>
2022-09-11 15:05:57 +0800

    edit index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 
   <title>{{ config('app.name', 'Laravel') }}</title>
@@ -12,7 +13,6 @@
 
 <body>
   <div id="app">
-    <p>Hello</p>
   </div>
   <router-view />
 </body>
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import path from 'path';
 
 export default defineConfig({
     server: {
-        // hmr: {
-        host: 'localhost',
-        // https: true,
-        // host: '0.0.0.0'
-        // },
+        hmr: {
+            host: 'localhost',
+            // https: true,
+            // host: '0.0.0.0'
+        },
     },
     plugins: [
         vue({
