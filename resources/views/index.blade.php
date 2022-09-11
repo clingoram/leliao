9a1d6ef208c1a95758ce5631cb1723b6ecbb63c7
Mavis <llingchen.27@gmail.com>
2022-09-11 15:17:42 +0800

    edit blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,6 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
-
   <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
 
