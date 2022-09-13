a26147e3396bfff2a86bb1fdfb0918c9d0fa3f64
Mavis <llingchen.27@gmail.com>
2022-09-13 16:10:42 +0800

    try to fix cors

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
+  <script src="https://kit.fontawesome.com/d3efb0ec.js" crossorigin="anonymous"></script>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
