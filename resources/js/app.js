4ae68c0518c294d7051b8faceea7d807de8dc277
Mavis <llingchen.27@gmail.com>
2022-09-13 10:33:55 +0800

    新增 import fontawesome至app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,13 @@ import { createApp } from 'vue';
 import router from './router/router.js';
 import App from './components/app.vue';
 
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/v4-shims.scss';
+import "@fortawesome/fontawesome-free/css/all.css";
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
