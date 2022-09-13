160cd6f5a4ec66e35779f10f67af57a8828901ec
Mavis <llingchen.27@gmail.com>
2022-09-13 10:37:13 +0800

    測試，先把ap.js部分code註解掉

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,21 @@
 // main file to run vue 3.
 import './bootstrap';
 
-import '../sass/app.scss';
+// import '../sass/app.scss';
 
-// vue
-import { createApp } from 'vue';
-import router from './router/router.js';
-import App from './components/app.vue';
+// // vue
+// import { createApp } from 'vue';
+// import router from './router/router.js';
+// import App from './components/app.vue';
 
-import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
-import '@fortawesome/fontawesome-free/scss/brands.scss';
-import '@fortawesome/fontawesome-free/scss/regular.scss';
-import '@fortawesome/fontawesome-free/scss/solid.scss';
-import '@fortawesome/fontawesome-free/scss/v4-shims.scss';
-import "@fortawesome/fontawesome-free/css/all.css";
+// // font-awesome
+// import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+// import '@fortawesome/fontawesome-free/scss/brands.scss';
+// import '@fortawesome/fontawesome-free/scss/regular.scss';
+// import '@fortawesome/fontawesome-free/scss/solid.scss';
+// import '@fortawesome/fontawesome-free/scss/v4-shims.scss';
+// import "@fortawesome/fontawesome-free/css/all.css";
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+// const app = createApp(App);
+// app.use(router);
+// app.mount('#app');
