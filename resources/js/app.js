c387a52e33231d1cda9350380dabb72c41fc9524
Mavis <llingchen.27@gmail.com>
2022-09-16 14:52:02 +0800

    把import scss的檔案註解

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,27 +10,10 @@ import router from './router/router.js';
 import App from './components/app.vue';
 
 /* font-awesome */
-
-// import '@fortawesome/fontawesome-free/webfonts/regular.min.js';
-
-// import '@fortawesome/fontawesome-free/scss/all.scss';
-// import '@fortawesome/fontawesome-free/scss/regular.scss';
-// import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
-// import '@fortawesome/fontawesome-free/scss/brands.scss';
-// import '@fortawesome/fontawesome-free/scss/solid.scss';
-// import '@fortawesome/fontawesome-free/scss/v4-shims.scss';
-
-// import '@fortawesome/fontawesome-free/js/all';
-
 import '@fortawesome/fontawesome-free/js/fontawesome.js';
 import '@fortawesome/fontawesome-free/js/all.js';
 import '@fortawesome/fontawesome-free/js/regular.js';
 import '@fortawesome/fontawesome-free/js/solid.js';
-
-
-
-// import '../font-awesome/css/all.min.css';
-// import '../font-awesome/css/regular.min.css';
 
 
 const app = createApp(App);
