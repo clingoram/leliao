0d61abae9b6a2272fe6c8e96be4d56aae21bce61
Mavis <llingchen.27@gmail.com>
2022-09-16 16:16:37 +0800

    heroku UI頁面顯示完成，剩下資料庫

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -71,8 +71,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
-        // add
-        // 'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
