70125f1585c1e75c2a6042edcce8ecbf830d5174
Mavis <llingchen.27@gmail.com>
2022-09-17 14:57:07 +0800

    edit database.php to add postgres

--- config/database.php
+++ config/database.php
@@ -69,20 +69,20 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            // TO-DO : connect to heroku postgres database
-            // 'host' => $DATABASE_URL["host"],
-            // 'port' => $DATABASE_URL["port"],
-            // 'database' => ltrim($DATABASE_URL["path"], "/"),
-            // 'username' => $DATABASE_URL["user"],
-            // 'passwrod' => $DATABASE_URL["pass"],
+            // connect to heroku postgres database
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'passwrod' => $DATABASE_URL["pass"],
 
             // localhost
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'url' => env('DATABASE_URL'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '5432'),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
 
             'charset' => 'utf8',
             'prefix' => '',
