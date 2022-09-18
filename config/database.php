1eeec817d9928646c1a8cd39dc0e2e7e371453a4
Mavis <llingchen.27@gmail.com>
2022-09-19 07:44:57 +0800

    edit conditional in database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Str;
 
 // local
-$url = env('DATABASE_URL');
+// $url = env('DATABASE_URL');
 $host = env('DB_HOST', '127.0.0.1');
 $port = env('DB_PORT', '5432');
 $database = env('DB_DATABASE', 'forge');
