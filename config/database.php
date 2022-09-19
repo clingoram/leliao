4e1257a25ac54d05feef55a36bdd270d74ddee0e
Mavis <llingchen.27@gmail.com>
2022-09-19 08:02:21 +0800

    edit conditional in database.php

--- config/database.php
+++ config/database.php
@@ -4,14 +4,15 @@ use Illuminate\Support\Str;
 
 // local
 // $url = env('DATABASE_URL');
-$host = env('DB_HOST', '127.0.0.1');
-$port = env('DB_PORT', '5432');
-$database = env('DB_DATABASE', 'forge');
-$username = env('DB_USERNAME', 'forge');
-$password = env('DB_PASSWORD', '');
-$sslmode = 'prefer';
+// $host = env('DB_HOST', '127.0.0.1');
+// $port = env('DB_PORT', '5432');
+// $database = env('DB_DATABASE', 'forge');
+// $username = env('DB_USERNAME', 'forge');
+// $password = env('DB_PASSWORD', '');
+// $sslmode = 'prefer';
 
 // $databaseUrl = env('DATABASE_URL');
+
 if (env('DATABASE_URL') === parse_url(getenv('DATABASE_URL'))) {
 
     // connect to heroku postgres
@@ -25,6 +26,15 @@ if (env('DATABASE_URL') === parse_url(getenv('DATABASE_URL'))) {
     $prefix = '';
     $schema = 'public';
     $sslmode = 'require';
+} else {
+    // local
+    $url = env('DATABASE_URL');
+    $host = env('DB_HOST', '127.0.0.1');
+    $port = env('DB_PORT', '5432');
+    $database = env('DB_DATABASE', 'forge');
+    $username = env('DB_USERNAME', 'forge');
+    $password = env('DB_PASSWORD', '');
+    $sslmode = 'prefer';
 }
 
 return [
@@ -117,7 +127,7 @@ return [
 
 
 
-            // 'url' => $url,
+            'url' => $url,
             'host' => $host,
             'port' => $port,
             'database' => $database,
