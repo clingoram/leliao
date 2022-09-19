063d4edb433c333de246613763f413ea29edb98a
Mavis <llingchen.27@gmail.com>
2022-09-19 08:39:50 +0800

    刪除註解的header

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,12 +23,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // 把token加到header內
 window.axios.defaults.headers.common['Authorization'] = `Bearer ${sessionStorage.getItem("token")}`;
 window.axios.defaults.withCredentials = true;
-// window.axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://leliao/';
-
-// window.axios.defaults.headers.post['xsrfCookieName'] = 'CSRFToken';
-// window.axios.defaults.headers.post['xsrfHeaderName'] = 'X-CSRFToken';
-// window.axios.defaults.headers.post['responseType'] = 'json';
-// window.axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 
 /**
